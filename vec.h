07a21d565794112bcf32cfbfe7d9090fff88947a
Adi Mehrotra <adimehrotra@Adi-Mehrotra.local>
2021-03-15 14:51:13 +0800

    repalced __ with _ for protected methods and added #pragma once guards to vec.h

--- vec.h
+++ vec.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <assert.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -9,27 +11,27 @@ typedef struct
 	size_t size;
 	size_t len;
 	size_t cap;
-} __CU_vec_info;
+} _CU_vec_info;
 
-#define __CU_get_vec_info_ptr(vec) ((__CU_vec_info*) (((void*) vec) - sizeof(__CU_vec_info)))
+#define _CU_get_vec_info_ptr(vec) ((_CU_vec_info*) (((void*) vec) - sizeof(_CU_vec_info)))
 
-#define CU_vec_init(T, n) ((T*) __CU_vec_init(sizeof(T), n))
-#define CU_vec_free(vec) (__CU_vec_free(__CU_get_vec_info_ptr(vec)))
+#define CU_vec_init(T, n) ((T*) _CU_vec_init(sizeof(T), n))
+#define CU_vec_free(vec) (_CU_vec_free(_CU_get_vec_info_ptr(vec)))
 
-#define CU_vec_len(vec) ((__CU_get_vec_info_ptr(vec))->len)
-#define CU_vec_cap(vec) ((__CU_get_vec_info_ptr(vec))->cap)
-#define CU_subvec(vec, start, end) __CU_subvec(vec, start, end)
+#define CU_vec_len(vec) ((_CU_get_vec_info_ptr(vec))->len)
+#define CU_vec_cap(vec) ((_CU_get_vec_info_ptr(vec))->cap)
+#define CU_subvec(vec, start, end) _CU_subvec(vec, start, end)
 
 #define CU_vec_reserve(vec, new_size)                                                              \
 	do                                                                                         \
 	{                                                                                          \
 		if (CU_vec_cap(vec) < new_size)                                                    \
 		{                                                                                  \
-			__CU_vec_info* info_ptr = __CU_get_vec_info_ptr(vec);                      \
+			_CU_vec_info* info_ptr = _CU_get_vec_info_ptr(vec);                        \
 			info_ptr = realloc(                                                        \
-				info_ptr, sizeof(__CU_vec_info) + new_size * info_ptr->size);      \
+				info_ptr, sizeof(_CU_vec_info) + new_size * info_ptr->size);       \
 			info_ptr->cap = new_size;                                                  \
-			vec = ((void*) info_ptr) + sizeof(__CU_vec_info);                          \
+			vec = ((void*) info_ptr) + sizeof(_CU_vec_info);                           \
 		}                                                                                  \
 	}                                                                                          \
 	while (false)
@@ -45,61 +47,66 @@ typedef struct
 #define CU_vec_push(vec, n)                                                                        \
 	do                                                                                         \
 	{                                                                                          \
-		__CU_vec_info* info_ptr = __CU_get_vec_info_ptr(vec);                              \
+		_CU_vec_info* info_ptr = _CU_get_vec_info_ptr(vec);                                \
 		info_ptr->len++;                                                                   \
-		info_ptr = __CU_vec_should_realloc(info_ptr);                                      \
-		vec = (void*) info_ptr + sizeof(__CU_vec_info);                                    \
+		info_ptr = _CU_vec_should_realloc(info_ptr);                                       \
+		vec = (void*) info_ptr + sizeof(_CU_vec_info);                                     \
 		vec[info_ptr->len - 1] = n;                                                        \
 	}                                                                                          \
 	while (false)
 
 #define CU_vec_pop(vec)                                                                            \
 	({                                                                                         \
-		__CU_vec_info* info_ptr = __CU_get_vec_info_ptr(vec);                              \
+		_CU_vec_info* info_ptr = _CU_get_vec_info_ptr(vec);                                \
 		info_ptr->len--;                                                                   \
 		vec[info_ptr->len];                                                                \
 	})
 
+#define CU_vec_popn(vec, n)                                                                        \
+	{                                                                                          \
+		_CU_vec_info* info_ptr = _CU_get_vec_info_ptr(vec);                                \
+		info_ptr->len -= n;                                                                \
+	}
+
 #define CU_vec_foreach(i, vec, body)                                                               \
 	do                                                                                         \
 	{                                                                                          \
-		for (int __CU_foreach_index = 0; __CU_foreach_index < CU_vec_len(vec);             \
-			__CU_foreach_index++)                                                      \
+		for (int _CU_foreach_index = 0; _CU_foreach_index < CU_vec_len(vec);               \
+			_CU_foreach_index++)                                                       \
 		{                                                                                  \
-			typeof(*vec) i = vec[__CU_foreach_index];                                  \
+			typeof(*vec) i = vec[_CU_foreach_index];                                   \
 			body;                                                                      \
 		}                                                                                  \
 	}                                                                                          \
 	while (false)
 
-void* __CU_vec_init(size_t size, int num)
+void* _CU_vec_init(size_t size, int num)
 {
-	void* alloc_ptr = malloc(sizeof(__CU_vec_info) + size * num);
-	*(__CU_vec_info*) alloc_ptr = (__CU_vec_info){size, num, num};
-	return alloc_ptr + sizeof(__CU_vec_info);
+	void* alloc_ptr = malloc(sizeof(_CU_vec_info) + size * num);
+	*(_CU_vec_info*) alloc_ptr = (_CU_vec_info){size, num, num};
+	return alloc_ptr + sizeof(_CU_vec_info);
 }
 
-void* __CU_subvec(void* vec, size_t start, size_t end)
+void* _CU_subvec(void* vec, size_t start, size_t num_elems)
 {
-	__CU_vec_info* vec_info = __CU_get_vec_info_ptr(vec);
-	int num_elems = end - start;
-	void* ret = __CU_vec_init(vec_info->size, num_elems);
+	_CU_vec_info* vec_info = _CU_get_vec_info_ptr(vec);
+	void* ret = _CU_vec_init(vec_info->size, num_elems);
 	memcpy(ret, vec + vec_info->size * start, vec_info->size * (num_elems));
 	return ret;
 }
 
-void __CU_vec_free(__CU_vec_info* vec)
+void _CU_vec_free(_CU_vec_info* vec)
 {
 	free(vec);
 }
 
-__CU_vec_info* __CU_vec_should_realloc(__CU_vec_info* vec)
+_CU_vec_info* _CU_vec_should_realloc(_CU_vec_info* vec)
 {
 	if (vec->len >= vec->cap)
 	{
 
 		vec->cap = (vec->cap * 3 / 2) + 1;
-		vec = realloc(vec, vec->cap * vec->size + sizeof(__CU_vec_info));
+		vec = realloc(vec, vec->cap * vec->size + sizeof(_CU_vec_info));
 	}
 
 	return vec;
